649cb1ea387c13414ac795f983d21531c2478788
nrser <neil@ztkae.com>
2016-10-17 16:45:44 +0800

    remove unused / unworking stuff inherited from babel-plugin-trace

--- test/fixtures/if-statement.js
+++
@@ -1,9 +0,0 @@
-export default function demo () {
-  trace: "hello world";
-  if (Math.random() > 0.5) {
-    trace: "Got result.";
-  }
-  else {
-    trace: "Got result."
-  }
-}
--- test/fixtures/multiple.js
+++
@@ -1,6 +0,0 @@
-export default function demo () {
-  trace: {
-    "hello world";
-    "foo bar";
-  }
-}
